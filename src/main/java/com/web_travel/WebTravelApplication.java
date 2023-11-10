1ee69c87c851e7ceb52557a907a766999e30d7ea
Miłosz Gmerek <milosz.gmerek6@gmail.com>
2023-11-10 10:18:30 +0100

    0.1: Create project files using spring initilizer

---
+++ src/main/java/com/web_travel/WebTravelApplication.java
@@ -0,0 +1,13 @@
+package com.web_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTravelApplication.class, args);
+    }
+
+}
